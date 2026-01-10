a6e459716d946b86850966e758198837bf779461
Piyush Raj <piyushrajbeg123@gmail.com>
2026-01-10 15:23:42 +0530

    Fix case-sensitive import issue for Vercel build

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,8 @@ import TermsAndConditions from './pages/TermsAndCondition'
 import Support from './pages/Support'
 
 import Checkout from "./pages/Checkout"
-import Success from "./pages/Success"
+// import Success from "./pages/Success"
+import Success from "./pages/success"
 
 
 import Login from "./pages/Login";
